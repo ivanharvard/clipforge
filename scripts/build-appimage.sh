39a33b98e1372fd8b5d3360304bc5c3574b586af
ivanharvard <ivangutierrez@college.harvard.edu>
2026-07-29 04:19:44 -0400

    fix appimage build

--- scripts/build-appimage.sh
+++ scripts/build-appimage.sh
@@ -7,6 +7,18 @@ cd "$(dirname "$0")/.."
 
 cargo build --release --bin clipforge-app
 
+rm -rf AppDir
+mkdir -p AppDir/usr/bin AppDir/usr/share/applications
+cp target/release/clipforge-app AppDir/usr/bin/
+cp packaging/linux/appimage/clipforge.desktop \
+  AppDir/usr/share/applications/com.clipforge.ClipForge.desktop
+
+for size in 16 24 32 48 64 128 256 512; do
+  icon_dir="AppDir/usr/share/icons/hicolor/${size}x${size}/apps"
+  mkdir -p "$icon_dir"
+  cp "packaging/shared/icons/clipforge-${size}.png" "$icon_dir/clipforge.png"
+done
+
 mkdir -p dist
 appimage-builder \
   --recipe packaging/linux/appimage/AppImageBuilder.yml \
